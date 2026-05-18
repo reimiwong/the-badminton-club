a3a00a0251fa63a5cd167fa24eb36ad3cc3179a1
Raymond Wong <Raymond.Wong@fsp.co>
2026-05-18 16:03:10 +0100

    allow any localhost to use cors

--- the-badminton-club-backend/src/server.ts
+++ the-badminton-club-backend/src/server.ts
@@ -11,27 +11,34 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
-  "http://localhost:5173",
   "https://shamrockbadminton.co.uk",
   "https://www.shamrockbadminton.co.uk",
   "https://icy-pond-0b7ff0910.7.azurestaticapps.net",
 ];
 
+
 app.use(
   cors({
     origin: (origin, callback) => {
-      // Allow non-browser tools (Postman, curl)
       if (!origin) return callback(null, true);
+
+      if (
+        origin.startsWith("http://localhost") ||
+        origin.startsWith("http://127.0.0.1")
+      ) {
+        return callback(null, true);
+      }
 
       if (allowedOrigins.includes(origin)) {
         return callback(null, true);
       }
 
-      return callback(new Error("Not allowed by CORS"));
+      return callback(null, false);
     },
     credentials: true,
-  }),
+  })
 );
+
 
 /* MIDDLEWARE ORDER (CRITICAL) */
 app.set("trust proxy", 1);
